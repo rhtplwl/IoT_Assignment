ce1ab7fb5191e1996ee3f317a193f74a8617c272
rhtplwl <paliwal.1@iitj.ac.in>
2018-08-19 22:42:16 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_alert(sender, receiver, password, smtpserver, port, ):
+	smtp = smtplib.SMTP(smtpserver, port)
+	smtp.ehlo()
+	smtp.starttls();
+	smtp.ehlo
+	smtp.login(sender, password)
+	msg = MIMEMultipart()
+	msg['From'] = sender
+	msg['To'] = receiver
+	msg['Subject'] :'IoT Alert'
+
+	body = "Your sensor is not running properly. Please Check!"
+	msg.attach(MIMEText(body, 'plain'))
+	text = msg.as_string()
+	smtp.sendmail(sender, receiver,text)
+	print("Alert Sent to " + receiver)
+	smtp.close()
--- mainFile.py
+++ mainFile.py
@@ -1,21 +1,35 @@
+#import required modules
 import sys
 import json
 import time
 import random
 import paho.mqtt.client as mqtt
+from mail import send_alert
 
+#callbacks definations
+
+#Callback on connection to the broker and to subscribe
 def on_connect(client, userdata, flags, rc):
     print("Connected with rc :" + str(rc))
-    client.subscribe(topics)
+    client.subscribe(topic)
 
-def on_message( client, userdata, msg):
-    print(str(msg.payload))
+#Callback to retrieve the msg from broker
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload)
+    sensor_type = payload.get("type")
+    min_value, max_value = payload.get("range")
+    value = payload.get("value")
+    print("Data recieved : " + str(msg.payload))
+    if value not in range(min_value, max_value):
+        print("Value of "+ sensor_type + " is out of range. Value : " + str(value))
+        send_alert(sender, receiver, login_pass, smtpserver, smtpport)
 
 
-
+#Importing Data from the config.json file.
 with open(sys.argv[1]) as handle:
     config = json.load(handle)
     mqtt_config = config.get("mqtt", {})
+    mail_config = config.get("mail", {})
     misc_config = config.get("misc", {})
     sensors = config.get("sensors")
 
@@ -23,7 +37,7 @@ with open(sys.argv[1]) as handle:
     verbose = misc_config.get("verbose", False)
 
     if not sensors:
-        print("no sensors specified in config, nothing to do")
+        print("No sensors specified.")
         exit()
 
     host = mqtt_config.get("host")
@@ -32,22 +46,26 @@ with open(sys.argv[1]) as handle:
     password = mqtt_config.get("password")
     topic = mqtt_config.get("topic")
 
-client = mqtt.Client()
-client.on_connect = on_connect
+    sender = mail_config.get("sendermail")
+    receiver = mail_config.get("receivermail")
+    login_pass = mail_config.get("password")
+    smtpserver = mail_config.get("smtpserver")
+    smtpport = mail_config.get("port")
 
-# if username:
-#     client.username_pw_set(username, password)
 
-client.username_pw_set(username, password)
-client.connect(host, port)
+client = mqtt.Client() #Client object
+print("Connecting : ")
+client.username_pw_set(username, password) #authentication
+client.connect(host, port) #connecting to the broker
 
 
 keys = list(sensors.keys())
 
+client.loop_start()
 while True:
     sensor_id = random.choice(keys)
     sensor = sensors[sensor_id]
-    min_val, max_val = sensor.get("range", [0, 100])
+    min_val, max_val = sensor.get("range")
     val = random.randint(min_val, max_val)
 
     data = {
@@ -55,18 +73,20 @@ while True:
         "value": val
     }
 
-    for key in ["unit", "type", "description"]:  
+    for key in ["unit", "type","range","description"]:  
         value = sensor.get(key)
 
         if value is not None:
             data[key] = value
 
+
     payload = json.dumps(data)
-
-    if verbose:
-        print("%s: %s" % (topic, payload))
-
+    client.on_connect = on_connect
+    client.on_message = on_message
+    time.sleep(interval)
     client.publish(topic, payload)
     time.sleep(interval)
 
+client.disconnect()
+client.loop_stop()
 
